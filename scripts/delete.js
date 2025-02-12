90708ce235a9c7aceac23960b1cb876cbeb7c4d4
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-02-12 16:34:17 +0900

    Update delete.js

--- scripts/delete.js
+++ scripts/delete.js
@@ -1,5 +1,6 @@
 function deleteCookie(name) {
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Lax`;
+    location.reload();
 }
 
 function showNotification(message) {
